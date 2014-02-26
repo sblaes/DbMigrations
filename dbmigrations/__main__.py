2707ada86d034e39e6da019e314f5b701eeda7dc
Zeal Jagannatha <zeal@skybox.com>
2014-02-26 14:56:35 -0800

    Added test CLI.

--- dbmigrations/__main__.py
+++ dbmigrations/__main__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
-import creator, applier, config, settings, versionupgrader
+import creator, applier, config, settings, versionupgrader, migrationtester
 from sys import argv
 
 parser = None
 applyparser = None
 createparser = None
+
 
 def main():
     parser = makeOptionParser()
@@ -15,9 +16,10 @@ def main():
         args.parser.print_help()
         return
     else:
-        if(args.prefix != None):
+        if args.prefix is not None:
             settings.ENVIRONMENT_PREFIX = args.prefix
         args.func(args)
+
 
 def versionMain(args):
     print('DbMigrations ' + settings.VERSION)
@@ -37,17 +39,20 @@ def makeOptionParser():
     combineParsers(subparsers, 'apply', applier.main, applier.initOptionParser)
     combineParsers(subparsers, 'upgrade-versions', versionupgrader.main, versionupgrader.initOptionParser)
     combineParsers(subparsers, 'version', versionMain)
+    combineParsers(subparsers, 'test', migrationtester.main, migrationtester.initOptionParser)
 
     if(settings.PRINT_CONFIG_ENABLED):
         combineParsers(subparsers, 'print-config', config.main, config.initOptionParser)
     return parser
 
+
 def combineParsers(subparsers, name, main, initParser=None):
     sub = subparsers.add_parser(name, add_help=False)
-    if(initParser != None):
+    if initParser is not None:
         initParser(sub)
     sub.add_argument("--help", dest="help", action="store_true", help="show this help message and exit")
     sub.set_defaults(func=main, parser=sub)
+
 
 if(__name__ == "__main__"):
     main()
